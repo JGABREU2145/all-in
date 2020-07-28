31d8b275de30a583f7fbc53decd92213fe221b7a
Justin Abreu <justinabreu92@gmail.com>
2020-07-27 22:06:58 -0500

    Added Nav component to home, need to get it function with values clicked

--- src/client/components/Nav/Nav.js
+++ src/client/components/Nav/Nav.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 
-export default function MenuPopupState() {
+export default function MenuPopupState(props) {
   return (
     <PopupState variant="popover" popupId="demo-popup-menu">
       {(popupState) => (
         <React.Fragment>
-          <Button variant="contained" color="primary" {...bindTrigger(popupState)}>
+          <Button
+            variant="contained"
+            color="primary"
+            {...bindTrigger(popupState)}
+          >
             View Ladder by Race
           </Button>
           <Menu {...bindMenu(popupState)}>
-            <MenuItem onClick={popupState.close} className='terran'>Terran</MenuItem>
-            <MenuItem onClick={popupState.close} className='protoss'>Protoss</MenuItem>
-            <MenuItem onClick={popupState.close} className='zerg'>Zerg</MenuItem>
-            <MenuItem onClick={popupState.close} className='all'>All</MenuItem>
+            <MenuItem
+              onClick={() => {
+                // console.log("TERRAN!");
+                props.setRace("terran");
+                popupState.close();
+              }}
+              className="terran"
+            >
+              Terran
+            </MenuItem>
+            <MenuItem onClick={popupState.close} className="protoss">
+              Protoss
+            </MenuItem>
+            <MenuItem onClick={popupState.close} className="zerg">
+              Zerg
+            </MenuItem>
+            <MenuItem onClick={popupState.close} className="all">
+              All
+            </MenuItem>
           </Menu>
         </React.Fragment>
       )}
--- src/client/components/home/home.js
+++ src/client/components/home/home.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, CircularProgress, Typography } from "@material-ui/core/";
+import Nav from "../Nav/Nav";
 import useClient from "../../hooks";
 import { map, slice } from "lodash";
 
 const Home = () => {
+  const [race, setRace] = useState("");
   const { loading, data } = useClient(
     "https://us.api.blizzard.com/sc2/ladder/grandmaster/1"
   );
+
+  useEffect(() => {
+    console.log("THE RACE IS NOW", race);
+  }, [race]);
   return loading ? (
     <CircularProgress />
   ) : (
     <div>
+      <Nav />
       {map(slice(data.ladderTeams, 0, 100), (team, index) => (
         <Card style={{ margin: "10px" }} key={index}>
           <Typography>Name: {team.teamMembers[0].displayName}</Typography>
